1f317d9d65fad292d7a5b7b0b864e43ce32e0aa5
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2015-04-27 17:14:56 +0200

    Gutschein aus HashMap lesen

--- app/src/main/java/ch/hslu/mobpro/bonusreminder/app/ScanResultsReceiver.java
+++ app/src/main/java/ch/hslu/mobpro/bonusreminder/app/ScanResultsReceiver.java
@@ -5,20 +5,28 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
-import android.util.Log;
 import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ScanResultsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+        Map<String, String> ssidBonus = new HashMap<String, String>();
+        ssidBonus.put("hslu", "Mensagutschein");
+        ssidBonus.put("IrgendeinAccessPoint", "Irgendein Gutschein");
+
         for (ScanResult scanResult : wifiManager.getScanResults()) {
-            if(scanResult.SSID.toLowerCase().equals("hslu")) {
-                Toast.makeText(context, "SSID: " + scanResult.SSID, Toast.LENGTH_LONG).show();
+            String ssid = scanResult.SSID.toLowerCase();
+            if (ssidBonus.containsKey(ssid)) {
+                String bonus = ssidBonus.get(ssid);
+                Toast.makeText(context, "Folgender Gutschein zücken: " + bonus, Toast.LENGTH_LONG).show();
                 break;
             }
         }
-
     }
 }
