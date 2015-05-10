58c222a2b927ab9cf8ef4e00b008f3aeec756808
Jonas Burri <j.burri@hotmail.com>
2015-05-10 21:23:37 +0200

    Revert "GUI"
    This reverts commit e4290209ec768e376a026cab2cb47f6f82269e64.

--- app/src/main/java/ch/hslu/mobpro/bonusreminder/app/ScanResultsService.java
+++ app/src/main/java/ch/hslu/mobpro/bonusreminder/app/ScanResultsService.java
@@ -5,11 +5,9 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.IBinder;
-import android.preference.PreferenceManager;
 
 import java.util.*;
 
@@ -59,20 +57,8 @@ public class ScanResultsService extends Service {
 
     private Map<String, String> getMap() {
         HashMap<String, String> map = new HashMap<String, String>();
-        /*map.put("hslu", "Mensagutschein");
-        map.put("UPC0039811", "Irgendein Gutschein");*/
-
-        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        final SharedPreferences.Editor editor = prefs.edit();
-        Set<String> boni = new HashSet<String>();
-        boni = prefs.getStringSet("boni", boni);
-        for(String bonus : boni){
-            String accessPoint = prefs.getString(bonus, "");
-            if(accessPoint!=""){
-                map.put(bonus, accessPoint);
-            }
-        }
-
+        map.put("hslu", "Mensagutschein");
+        map.put("UPC0039811", "Irgendein Gutschein");
         return map;
     }
 
