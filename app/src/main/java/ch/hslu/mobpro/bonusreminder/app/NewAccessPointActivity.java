c030ec8a70f8b61283f671e2c99d9e295182a056
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2015-05-11 14:16:34 +0200

    Refactor onClick

--- app/src/main/java/ch/hslu/mobpro/bonusreminder/app/NewAccessPointActivity.java
+++ app/src/main/java/ch/hslu/mobpro/bonusreminder/app/NewAccessPointActivity.java
@@ -5,37 +5,30 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
-/**
- * Created by Jonas on 07.05.2015.
- */
 public class NewAccessPointActivity extends Activity {
+
+    private String bonusName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_new_access_point);
 
-        final String bonusName = getIntent().getStringExtra("selected_bonus");
+        bonusName = getIntent().getStringExtra("selected_bonus");
+    }
 
-        Button button = (Button) findViewById(R.id.new_access_point_confirm);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText textField = (EditText) findViewById(R.id.new_access_point_textfield);
-                String newAccessPoint = textField.getText().toString();
+    public void saveAccessPoint(View view) {
+        EditText textField = (EditText) findViewById(R.id.new_access_point_textfield);
+        String newAccessPoint = textField.getText().toString();
 
-                final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(NewAccessPointActivity.this);
-                final SharedPreferences.Editor editor = prefs.edit();
-                editor.putString(bonusName, newAccessPoint);
-                editor.apply();
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(NewAccessPointActivity.this);
+        final SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(bonusName, newAccessPoint);
+        editor.apply();
 
-                finish();
-            }
-        });
-
+        finish();
     }
 
 
--- app/src/main/java/ch/hslu/mobpro/bonusreminder/app/NewBonusActivity.java
+++ app/src/main/java/ch/hslu/mobpro/bonusreminder/app/NewBonusActivity.java
@@ -5,41 +5,32 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * Created by Jonas on 07.05.2015.
- */
 public class NewBonusActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_new_bonus);
+    }
 
+    public void saveBonus(View view) {
+        EditText textField = (EditText) findViewById(R.id.new_bonus_textfield);
+        String newBonus = textField.getText().toString();
 
-        Button button = (Button) findViewById(R.id.new_bonus_confirm);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText textField = (EditText) findViewById(R.id.new_bonus_textfield);
-                String newBonus = textField.getText().toString();
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(NewBonusActivity.this);
+        final SharedPreferences.Editor editor = prefs.edit();
+        Set<String> boni = new HashSet<String>();
+        boni = prefs.getStringSet(MainActivity.BONUS_KEY, boni);
+        boni.add(newBonus);
 
-                final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(NewBonusActivity.this);
-                final SharedPreferences.Editor editor = prefs.edit();
-                Set<String> boni = new HashSet<String>();
-                boni = prefs.getStringSet("boni", boni);
-                boni.add(newBonus);
+        editor.putStringSet(MainActivity.BONUS_KEY, boni);
+        editor.apply();
 
-                editor.putStringSet("boni", boni);
-                editor.apply();
-
-                finish();
-            }
-        });
+        finish();
     }
 }
